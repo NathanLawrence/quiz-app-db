42c7aa72fedcfdfc732a79547287d28ffe3b475d
Nathan Lawrence <lawrencen@missouri.edu>
2015-09-21 15:12:48 -0500

    Initial commit

---
+++ retrieve-things.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<!--Stylesheets-->
+	<!--Eric Meyer's Brilliant CSS Reset 2.0-->
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<!--Basic site-wide stylesheet for fonts, etc.-->
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<!--"Branding" CSS for Colors and Logo information-->
+	<link rel="stylesheet" type="text/css" href="css/kbia-brand.css">
+	<title>Retrieve Data for the All Things Considered Quiz</title>
+</head>
+<body>
+
+</body>
+</html>
