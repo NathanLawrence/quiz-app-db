885dc0a018509ef51ef4ed8bfb47575558f652f0
Nathan Lawrence <lawrencen@missouri.edu>
2015-09-23 12:02:13 -0500

    Commenting is good and important.

--- register-morning.php
+++ register-morning.php
@@ -18,7 +18,8 @@
 				<li><p>First Name:</p><input type="text" name="firstname"></li>
 				<li><p>Last Name:</p><input type="text" name="lastname"></li>
 				<li><p>Email:</p><input type="email" name="email"></li>
-				<li><p>Number Correct:</p><input type="hidden" name="right" value="<?php echo $_GET["right"]; ?>"></li>
+				<!--This next one is kinda neato. I can pass the number of questions gotten right (submitted in JS by a get on the previous page) through to the post page.-->
+				<li><input type="hidden" name="right" value="<?php echo $_GET["right"]; ?>"></li>
 				<li><input type="submit" name="submit"></li>
 			</ul>
 		</form>
