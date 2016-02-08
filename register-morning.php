151793141cb17274b3698923f5c9fc0dea1ac91f
Nathan Lawrence <lawrencen@missouri.edu>
2016-02-08 09:36:46 -0600

    Make it pretty!

--- register-morning.php
+++ register-morning.php
@@ -15,14 +15,14 @@
 	<div class="container">
 		<!--<div id="kbiaHeader"><img src="http://media.kbia.org.s3.amazonaws.com/evergreen/KBIA_main_reverse.png"></div>-->
 		<h1>Register for the Morning Edition Quiz Giveaway</h1>
-		<form action="post-morning.php" method="post">
+		<form class="form-horizontal" action="post-morning.php" method="post">
 			<ul class="form-list">
-				<li><div class="form-group"><label for="firstname">First Name:</label><input type="text" class="form-control" name="firstname"></div></li>
-				<li><p>Last Name:</p><input type="text" class="form-control" name="lastname"></li>
-				<li><p>Email:</p><input type="email" class="form-control" name="email"></li>
+				<li><div class="form-group"><label class="control-label col-sm-2" for="firstname">First Name:</label><input type="text" class="form-control" name="firstname"></div></li>
+				<li><div class="form-group"><label class="control-label col-sm-2" for="lastname">Last Name:</label><input type="text" class="form-control" name="lastname"></div></li>
+				<li><div class="form-group"><label class="control-label col-sm-2" for="email">Email:</label><input type="email" class="form-control" name="email"></div></li>
 				<!--This next one is kinda neato. I can pass the number of questions gotten right (submitted in JS by a get on the previous page) through to the post page.-->
 				<li><input type="hidden" name="right" value="<?php echo $_GET["right"]; ?>"></li>
-				<li><div class="input-group"><input type="submit" class="btn btn-default btn-lg" name="submit"></div></li>
+				<li><div class="form-group"><div class="col-sm-offset-2 col-sm-10"><input type="submit" class="btn btn-primary btn-lg" name="submit"></div></div></li>
 			</ul>
 		</form>
 	</div>
